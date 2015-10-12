6f47761513c2642cb5eeea8b27825781b30af968
oscarsuen <suen.oscar@gmail.com>
2015-10-12 12:32:16 -0400

    added sql file

---
+++ library_2015-10-12.sql
@@ -0,0 +1,79 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4499
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 5.6.26)
+# Database: library
+# Generation Time: 2015-10-12 16:31:13 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table book
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `book`;
+
+CREATE TABLE `book` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ISBN` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `author` varchar(20) NOT NULL,
+  `shelf_status` tinyint(4) NOT NULL,
+  `call_no` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table loan
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `loan`;
+
+CREATE TABLE `loan` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `due_date` date NOT NULL,
+  `bID` int(11) NOT NULL,
+  `mID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `bID` (`bID`),
+  KEY `mID` (`mID`),
+  CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`bID`) REFERENCES `book` (`ID`),
+  CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`mID`) REFERENCES `member` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table member
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `debt` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
