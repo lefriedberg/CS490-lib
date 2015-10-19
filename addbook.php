008543f95099e4a89cd2bd709577a59f408ce9c3
xstephenhu98 <xstephenhu98@gmail.com>
2015-10-19 00:05:40 -0400

    Added alerts and styles for PHP output (addbook, addmember)

--- addbook.php
+++ addbook.php
@@ -1,3 +1,39 @@
+<html>
+<head>
+	<title>Library Database System</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Home</span></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="loan.html"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a></li>
+            <li><a href="addbook.html"><span class="glyphicon glyphicon-book" aria-hidden="true"></span></a></li>
+			<li><a href="addmember.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+			<li><a href="booksearch.html"><span class="glyphicon glyphicon-barcode" aria-hidden="true"></span></a></li>
+			<li><a href="membersearch.html"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+			<li><a href="return.html"><span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	<div class="container theme-showcase" role="main">
+		<div class="jumbotron">
+		
+
 <?php
 	if($_POST['submit'] == "Submit") {
 		
@@ -9,16 +45,37 @@
 		$varCall_no = $_POST['call_no'];
 
 		if(empty($varTitle)) {
-			$errorMessage .= "<li>Enter title</li>";
+			$errorMessage .= "Title";
+			echo "<div class='alert alert-danger' role='alert'>";
+			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Error:</span>";
+  			echo " You forgot to enter the title!";
+  			echo "</div>";
+
 		}
 		if(empty($varAuthor)) {
-			$errorMessage .= "<li>Enter author</li>";
+			$errorMessage .= "Author";
+			echo "<div class='alert alert-danger' role='alert'>";
+			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Error:</span>";
+  			echo " You forgot to enter the title!";
+  			echo "</div>";
 		}
 		if(empty($varISBN)) {
-			$errorMessage .= "<li>Enter ISBN</li>";
+			$errorMessage .= "ISBN";
+			echo "<div class='alert alert-danger' role='alert'>";
+			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Error:</span>";
+  			echo " You forgot to enter the title!";
+  			echo "</div>";
 		}
 		if(empty($varCall_no)) {
-			$errorMessage .= "<li>Enter call number</li>";
+			$errorMessage .= "Call_No";
+			echo "<div class='alert alert-danger' role='alert'>";
+			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Error:</span>";
+  			echo " You forgot to enter the title!";
+  			echo "</div>";
 		}
 		
 		if(empty($errorMessage)) {
@@ -35,13 +92,25 @@
 			mysql_query($sql);
 
 			$rollno = mysql_query("SELECT ID FROM book WHERE title='" . $varTitle . "' AND author='" . $varAuthor . "'");
+			
+
 			$row = mysql_fetch_array($rollno);
-			echo("Book ID: " . $row['ID']);
+			
+			echo "<div class='alert alert-success' role='alert'>";
+			echo "<span class='glyphicon glyphicon-ok-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Success</span>";
+  			echo " Form submitted!";
+  			echo "</div>";	
+
+			echo("<p>Book ID: " . $row['ID']. "</p>");
+			echo("</br>");
+
+			echo "<a href='addbook.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Submit Another Entry</a>";
 
 			exit();
 		} else {
-			echo($errorMessage);
-		}
+			echo "<a href='addbook.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Try Again</a>";
+			}
 
 	}
 	
@@ -63,3 +132,15 @@
 		return($value);
 	}
 ?>
+
+</div>
+	</div>
+	
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
+
--- addmember.php
+++ addmember.php
@@ -1,3 +1,39 @@
+<html>
+<head>
+	<title>Library Database System</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Home</span></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="loan.html"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a></li>
+            <li><a href="addbook.html"><span class="glyphicon glyphicon-book" aria-hidden="true"></span></a></li>
+			<li><a href="addmember.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+			<li><a href="booksearch.html"><span class="glyphicon glyphicon-barcode" aria-hidden="true"></span></a></li>
+			<li><a href="membersearch.html"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+			<li><a href="return.html"><span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	<div class="container theme-showcase" role="main">
+		<div class="jumbotron">
+		
+
 <?php
 	if($_POST['submit'] == "Submit") {
 		
@@ -6,7 +42,12 @@
 		$varName = $_POST['name'];
 
 		if(empty($varName)) {
-			$errorMessage .= "<li>Enter name</li>";
+			$errorMessage .= "Name";
+			echo "<div class='alert alert-danger' role='alert'>";
+			echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Error:</span>";
+  			echo " You forgot to enter the name!";
+  			echo "</div>";
 		}
 		
 		if(empty($errorMessage)) {
@@ -21,11 +62,20 @@
 
 			$memberid = mysql_query("SELECT ID FROM member WHERE name='" . $varName . "'");
 			$row = mysql_fetch_array($memberid);
-			echo("ID: " . $row['ID']);
+			
+			echo "<div class='alert alert-success' role='alert'>";
+			echo "<span class='glyphicon glyphicon-ok-sign' aria-hidden='true'></span>";
+			echo "<span class='sr-only'>Success</span>";
+  			echo " Form submitted!";
+  			echo "</div>";	
 
+			echo("<p>ID: " . $row['ID']. "</p>");
+			echo("</br>");
+
+			echo "<a href='addmember.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Submit Another Entry</a>";
 			exit();
 		} else {
-			echo($errorMessage);
+			echo "<a href='addmember.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Try Again</a>";;
 		}
 
 	}
