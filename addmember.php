eb303befc1ae52a075ea33c49586505b66c818a0
lefriedberg <lefriedberg@mail.ru>
2015-10-19 15:47:43 -0400

    comments in addmember

--- addmember.php
+++ addmember.php
@@ -41,7 +41,8 @@
 		$errorMessage = "";
 
 		$varName = $_POST['name'];
-
+        
+        //there must be a name
 		if(empty($varName)) {
 			$errorMessage .= "Name";
 			echo "<div class='alert alert-danger' role='alert'>";
@@ -54,14 +55,14 @@
 		if(empty($errorMessage)) {
 			$db = mysql_connect("localhost","root","");
 			
-			if(!$db) die("Error connecting to MySQL database.");
+			if(!$db) die("Error connecting to MySQL database."); //connect to database
 			mysql_select_db("library" ,$db);
 			
 			$sql = "INSERT INTO member (name) VALUES (".
 			PrepSQL($varName) . ")";
 			mysql_query($sql);
 
-			$memberid = mysql_query("SELECT ID FROM member ORDER BY id DESC LIMIT 1");
+			$memberid = mysql_query("SELECT ID FROM member ORDER BY id DESC LIMIT 1"); //prints id
 			$row = mysql_fetch_array($memberid);
 			
 			echo "<div class='alert alert-success' role='alert'>";
