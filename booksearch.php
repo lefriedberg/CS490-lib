511b2b6221962becbe7eaba1060391fc84065c71
xstephenhu98 <xstephenhu98@gmail.com>
2015-10-19 00:14:22 -0400

    Trying to style the table view for booksearch - does it work?

--- booksearch.php
+++ booksearch.php
@@ -1,3 +1,39 @@
+<html>
+<head>
+    <title>Library Database System</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Home</span></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="loan.html"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a></li>
+            <li><a href="addbook.html"><span class="glyphicon glyphicon-book" aria-hidden="true"></span></a></li>
+            <li><a href="addmember.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+            <li><a href="booksearch.html"><span class="glyphicon glyphicon-barcode" aria-hidden="true"></span></a></li>
+            <li><a href="membersearch.html"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+            <li><a href="return.html"><span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="container theme-showcase" role="main">
+        <div class="jumbotron">
+        
+
 <?php
     
     if($_POST["submit"] == "Submit") {
@@ -26,6 +62,11 @@
             
         if(empty($rql)) {
             $errorMessage = "Enter search terms!";
+            echo "<div class='alert alert-danger' role='alert'>";
+            echo "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>";
+            echo "<span class='sr-only'>Error:</span>";
+            echo " You forgot to enter the search terms!";
+            echo "</div>";
         }
             
         $rql = substr($rql, 0 , -5); //now we have a good query!
@@ -44,7 +85,7 @@
 
             $result = mysql_query($sql);
             
-            echo "<table border = 1>";
+            echo "<table class='table table-striped'>";
             while($row= mysql_fetch_array($result))
                       echo("<tr><td>ID</td><td>Title</td><td>Author</td><td>ISBN</td><td>Call No</td><td>Shelf Status</td></tr><tr><td>".
                             $row['ID']."</td><td>".
@@ -61,7 +102,11 @@
               //  $i++;}
             
         //header("thank-you.html");
+            echo "<a href='booksearch.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Submit Another Entry</a>";
         exit();
+        }
+        else {
+            echo "<a href='booksearch.html' class='btn btn-primary' role='button'><span class='glyphicon glyphicon-repeat' aria-hidden='true'></span> Try Again</a>";
         } 
     }
 
@@ -80,3 +125,13 @@
         return($value);
     }
 ?>
+</div>
+    </div>
+    
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
